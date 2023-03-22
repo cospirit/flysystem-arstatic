8b3e9516971c41a895cf717416a440476372097b
Igor Zakhvatov <igor.zahvatov@gmail.com>
2023-03-22 14:35:23 +0300

    feat(symfony-upgrade): Updated league/flysystem to 3.12

--- src/ArStatic.php
+++ src/ArStatic.php
@@ -1,31 +1,37 @@
 <?php
 namespace CoSpirit\Flysystem\Adapter;
 
-use League\Flysystem\AdapterInterface;
+use CurlHandle;
 use League\Flysystem\Config;
-use League\Flysystem\NotSupportedException;
-use League\Flysystem\Util;
+use League\Flysystem\FileAttributes;
+use League\Flysystem\FilesystemAdapter;
+use League\Flysystem\UnableToDeleteFile;
+use League\Flysystem\UnableToReadFile;
+use League\Flysystem\UnableToWriteFile;
+use League\MimeTypeDetection\MimeTypeDetector;
 
-class ArStatic implements AdapterInterface
+class ArStatic implements FilesystemAdapter
 {
-    /**
-     * @var string
-     */
-    protected $apiUrl;
-
-    /**
-     * @var string
-     */
-    protected $application;
-
-    /**
-     * @param string $apiUrl
-     * @param string $application
-     */
-    public function __construct($apiUrl, $application)
-    {
+    public function __construct(
+        protected string $apiUrl,
+        protected string $application,
+        private readonly MimeTypeDetector $mimeTypeDetector
+    ) {
         $this->apiUrl = rtrim($apiUrl, '/');
-        $this->application = $application;
+    }
+
+    public function getAbsolutePath(string $slug = ''): string|false
+    {
+        $absolutePath = sprintf('%s/%s/', $this->apiUrl, $this->application);
+        if (!empty($slug)) {
+            if ($this->fileExists($slug)) {
+                return $absolutePath . $slug;
+            }
+
+            return false;
+        }
+
+        return $absolutePath;
     }
 
     /**
@@ -41,108 +47,45 @@ class ArStatic implements AdapterInterface
 
     /**
      * Write a new file.
-     *
-     * @param string $path
-     * @param string $contents
-     * @param Config $config Config object
-     * @return array|false false on failure file meta data on success
      */
-    public function write($path, $contents, Config $config = null)
+    public function write(string $path, string $contents, Config $config): void
     {
         $tmpFile = tempnam(sys_get_temp_dir(), 'arconnect-static');
         file_put_contents($tmpFile, $contents);
 
         $file = new \CURLFile(
             $tmpFile,
-            Util::guessMimeType($path, $tmpFile),
+            $this->mimeTypeDetector->detectMimeTypeFromFile($tmpFile) ?? $this->mimeTypeDetector->detectMimeTypeFromPath($path),
             basename($tmpFile)
         );
-
-        $data = [
-            'slug' => $path,
-            'file' => $file,
-        ];
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->apiUrl.'/'.$this->application);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, [
+            'slug' => $path,
+            'file' => $file,
+        ]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_exec($ch);
-        $fileInfo = $this->getinfo($ch);
+        $fileInfo = $this->getInfo($ch);
         curl_close($ch);
 
         unlink($tmpFile);
 
-        if ($fileInfo['http_code'] >= 200 && $fileInfo['http_code'] < 400) {
-            $response = [
-                'contents' => $contents,
-                'type'     => $fileInfo['content_type'],
-                'size'     => $fileInfo['size_download'],
-                'path'     => $path,
-            ];
-
-            return $response;
+        if ($fileInfo['http_code'] < 200 || $fileInfo['http_code'] >= 400) {
+            throw UnableToWriteFile::atLocation($path, "Unexpected response status code: \"{$fileInfo['http_code']}\"");
         }
-
-        return false;
     }
 
     /**
      * Write a new file using a stream.
      *
-     * @param string $path
-     * @param resource $resource
-     * @param Config $config Config object
-     * @return array|false false on failure file meta data on success
+     * @param resource $contents
      *
      * @throws NotSupportedException
      */
-    public function writeStream($path, $resource, Config $config)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * Update a file.
-     *
-     * @param string $path
-     * @param string $contents
-     * @param Config $config Config object
-     * @return array|false false on failure file meta data on success
-     *
-     * @throws NotSupportedException
-     */
-    public function update($path, $contents, Config $config)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * Update a file using a stream.
-     *
-     * @param string $path
-     * @param resource $resource
-     * @param Config $config Config object
-     * @return array|false false on failure file meta data on success
-     *
-     * @throws NotSupportedException
-     */
-    public function updateStream($path, $resource, Config $config)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * Rename a file.
-     *
-     * @param string $path
-     * @param string $newpath
-     * @return bool
-     *
-     * @throws NotSupportedException
-     */
-    public function rename($path, $newpath)
+    public function writeStream(string $path, $contents, Config $config): void
     {
         throw new NotSupportedException();
     }
@@ -150,13 +93,9 @@ class ArStatic implements AdapterInterface
     /**
      * Copy a file.
      *
-     * @param string $path
-     * @param string $newpath
-     * @return bool
-     *
      * @throws NotSupportedException
      */
-    public function copy($path, $newpath)
+    public function copy(string $source, string $destination, Config $config): void
     {
         throw new NotSupportedException();
     }
@@ -165,45 +104,19 @@ class ArStatic implements AdapterInterface
      * Delete a file.
      *
      * @param string $path
-     * @return bool
      */
-    public function delete($path)
+    public function delete(string $path): void
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->apiUrl.'/'.$this->application.'/'.$path);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
         curl_exec($ch);
-        $response = $this->getinfo($ch, CURLINFO_HTTP_CODE);
+        $response = $this->getInfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
-        return ($response >= 200 && $response < 400) ? true : false;
-    }
-
-    /**
-     * Delete a directory.
-     *
-     * @param string $dirname
-     * @return bool
-     *
-     * @throws NotSupportedException
-     */
-    public function deleteDir($dirname)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * Create a directory.
-     *
-     * @param string $dirname directory name
-     * @param Config $config
-     * @return array|false
-     *
-     * @throws NotSupportedException
-     */
-    public function createDir($dirname, Config $config)
-    {
-        throw new NotSupportedException();
+        if ($response < 200 || $response >= 400) {
+            throw UnableToDeleteFile::atLocation($path, "Unexpected response status code: \"$response\"");
+        }
     }
 
     /**
@@ -211,80 +124,49 @@ class ArStatic implements AdapterInterface
      *
      * @param string $path
      * @param string $visibility
-     * @return array|false file meta data
      *
      * @throws NotSupportedException
      */
-    public function setVisibility($path, $visibility)
+    public function setVisibility(string $path, string $visibility): void
     {
         throw new NotSupportedException();
     }
 
     /**
-     * Check whether a file exists.
-     *
-     * @param string $path
-     * @return bool
-     */
-    public function has($path)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->apiUrl.'/'.$this->application.'/'.$path);
-        curl_setopt($ch, CURLOPT_NOBODY, 1);
-        curl_exec($ch);
-        $response = $this->getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        return ($response >= 200 && $response < 400) ? true : false;
-    }
-
-    /**
      * Read a file.
-     *
-     * @param string $path
-     * @return array|false
      */
-    public function read($path)
+    public function read(string $path): string
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->apiUrl.'/'.$this->application.'/'.$path);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $contents = curl_exec($ch);
+        $responseStatus = $this->getInfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
-        if ($contents === false) {
-            return false;
+        if ($responseStatus < 200 || $responseStatus >= 400) {
+            throw UnableToReadFile::fromLocation($path, "Unexpected response status code: \"$responseStatus\"");
         }
 
-        return [
-            'contents' => $contents,
-            'path'     => $path,
-        ];
+        return $contents;
     }
 
     /**
      * Read a file as a stream.
-     *
-     * @param string $path
-     * @return array|false
      */
-    public function readStream($path)
+    public function readStream(string $path)
     {
         throw new NotSupportedException();
     }
 
     /**
      * List contents of a directory.
-     *
-     * @param string $directory
-     * @param bool $recursive
-     * @return array
      */
-    public function listContents($directory = '', $recursive = false)
+    public function listContents(string $path, bool $deep): iterable
     {
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $this->apiUrl.'/'.$this->application.'/'.$directory.'/list');
+        curl_setopt($curl, CURLOPT_URL, $this->apiUrl.'/'.$this->application.'/'.$path.'/list');
         curl_setopt ($curl, CURLOPT_RETURNTRANSFER, true) ;
         curl_setopt ($curl, CURLOPT_TIMEOUT, '12');
 
@@ -295,98 +177,63 @@ class ArStatic implements AdapterInterface
         return $directories;
     }
 
-    /**
-     * Get all the meta data of a file or directory.
-     *
-     * @param string $path
-     * @return array|false
-     *
-     * @throws NotSupportedException
-     */
-    public function getMetadata($path)
+    public function fileExists(string $path): bool
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->apiUrl.'/'.$this->application.'/'.$path);
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_exec($ch);
+        $response = $this->getInfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $response >= 200 && $response < 400;
+    }
+
+    public function directoryExists(string $path): bool
+    {
+        return false;
+    }
+
+    public function deleteDirectory(string $path): void
+    {
+        throw new NotSupportedException();
+    }
+
+    public function createDirectory(string $path, Config $config): void
+    {
+        throw new NotSupportedException();
+    }
+
+    public function visibility(string $path): FileAttributes
+    {
+        throw new NotSupportedException();
+    }
+
+    public function mimeType(string $path): FileAttributes
+    {
+        throw new NotSupportedException();
+    }
+
+    public function lastModified(string $path): FileAttributes
+    {
+        throw new NotSupportedException();
+    }
+
+    public function fileSize(string $path): FileAttributes
+    {
+        throw new NotSupportedException();
+    }
+
+    public function move(string $source, string $destination, Config $config): void
     {
         throw new NotSupportedException();
     }
 
     /**
-     * Get all the meta data of a file or directory.
-     *
-     * @param string $path
-     * @return array|false
-     *
-     * @throws NotSupportedException
-     */
-    public function getSize($path)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * Get the mimetype of a file.
-     *
-     * @param string $path
-     * @return array|false
-     *
-     * @throws NotSupportedException
-     */
-    public function getMimetype($path)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * Get the timestamp of a file.
-     *
-     * @param string $path
-     * @return array|false
-     *
-     * @throws NotSupportedException
-     */
-    public function getTimestamp($path)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * Get the visibility of a file.
-     *
-     * @param string $path
-     * @return array|false
-     *
-     * @throws NotSupportedException
-     */
-    public function getVisibility($path)
-    {
-        throw new NotSupportedException();
-    }
-
-    /**
-     * @param string $slug
-     * @return bool|string
-     */
-    public function getAbsolutePath($slug = '')
-    {
-        $absolutePath = sprintf('%s/%s/', $this->apiUrl, $this->application);
-
-        if (!empty($slug)) {
-            if ($this->has($slug)) {
-                return $absolutePath . $slug;
-            }
-
-            return false;
-        }
-
-        return $absolutePath;
-    }
-
-    /**
-     * @param resource $ch
-     * @param int $opt [optional] <p>
-     *
      * @return mixed If opt is given, returns its value as a string.
      * Otherwise, returns an associative array.
      */
-    private function getinfo($ch, $opt = null)
+    private function getInfo(CurlHandle $ch, int $opt = null): mixed
     {
         if (curl_errno($ch)) {
             return false;
