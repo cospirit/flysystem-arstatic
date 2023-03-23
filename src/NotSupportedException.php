a6800e378a8e89861622cfbff869f7915048d968
Igor Zakhvatov <igor.zahvatov@gmail.com>
2023-03-23 19:13:10 +0300

    feat(symfony-upgrade): Applied php-cs-fixer rules

--- src/NotSupportedException.php
+++ src/NotSupportedException.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace CoSpirit\Flysystem\Adapter;
 
 use League\Flysystem\FilesystemException;
-use RuntimeException;
 
-class NotSupportedException extends RuntimeException implements FilesystemException
+class NotSupportedException extends \RuntimeException implements FilesystemException
 {
 }
